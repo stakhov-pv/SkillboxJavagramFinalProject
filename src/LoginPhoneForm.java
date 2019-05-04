73f0804e7e83d2809b6ececc995d27d6acdfe02e
stakhov-pv <stakhov.pv@gmail.com>
2019-05-05 02:39:23 +0500

    put background picture on form

--- src/LoginPhoneForm.java
+++ src/LoginPhoneForm.java
@@ -1,11 +1,34 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class LoginPhoneForm {
+    private JPanel rootPanel;
+    private BufferedImage background;
+
+    public LoginPhoneForm() {
+        try {
+            background = ImageIO.read(new File("res/background.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public JPanel getRootPanel() {
         return rootPanel;
     }
 
-    private JPanel rootPanel;
-
+    private void createUIComponents() {
+        rootPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics graphics) {
+                super.paintComponent(graphics);
+                graphics.drawImage(background,0,0, null);
+            }
+        };
+    }
 
 }
