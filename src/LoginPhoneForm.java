0c7aafebf8f22228bda014d1bdb92c117b649690
Paul <tsapree@gmail.com>
2019-05-09 23:10:18 +0500

    change button color, disable border for text field, added divider to phone

--- src/LoginPhoneForm.java
+++ src/LoginPhoneForm.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -16,6 +17,8 @@ public class LoginPhoneForm {
     private JPanel headerPanel;
     private JPanel backgroundPanel;
     private JButton continueButton;
+    private JLabel label1;
+    private JPanel phonePanel;
     private BufferedImage logo;
     private LoginFormListener listener;
 
@@ -44,15 +47,43 @@ public class LoginPhoneForm {
         //    }
         //};
 
+        Font lightFont = null;
+        try {
+            //create the font to use. Specify the size!
+            lightFont = Font.createFont(Font.TRUETYPE_FONT, new File("res/OpenSansLight.ttf"));
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            //register the font
+            ge.registerFont(lightFont);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch(FontFormatException e) {
+            e.printStackTrace();
+        }
+
         logoPanel = new JPanelImage("res/logo.png");
         phoneIcon = new JPanelImage("res/icon-phone.png");
         //phoneIcon = new JPanelImage("res/your-face.png");
         backgroundPanel = new JPanelImage("res/background.png");
         //continueButton = new JButtonImage("res/button-background.png");
+
+        phonePanel = new JPanel();
+        Border phoneBorder = BorderFactory.createMatteBorder(0,0,2,0,Color.WHITE);
+        phonePanel.setBorder(phoneBorder);
+
         ImageIcon imageForOne = new ImageIcon("res/button-background.png");
         continueButton = new JButton(imageForOne);
         continueButton.setBorderPainted(false);
+        continueButton.setContentAreaFilled(false);
+        continueButton.setFocusable(false);
+        continueButton.setFont(lightFont.deriveFont(28f));
 
+        phoneValue = new JTextField();
+        phoneValue.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        //phoneValue.setText("+7");
+        phoneValue.setFont(lightFont.deriveFont(44f));
+
+        label1 = new JLabel();
+        label1.setFont(lightFont.deriveFont(18f));
 
     }
 
