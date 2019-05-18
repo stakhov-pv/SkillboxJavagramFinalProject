8b74806a0cc4927f0d6be2acfcc730d890e18cda
Paul <tsapree@gmail.com>
2019-05-19 01:01:49 +0500

    main form enhancements

--- src/ConversationsForm.java
+++ src/ConversationsForm.java
@@ -30,6 +30,18 @@ public class ConversationsForm {
     private JPanel user1;
     private JPanel user1AvatarPanel;
     private JPanel user2;
+    private JLabel user1when;
+    private JLabel user1Name;
+    private JLabel user1LastMessage;
+    private JLabel searchLabel;
+    private JTextField messageTextField;
+    private JPanel messageButtonPanel;
+    private JPanel myMessagePanel;
+    private JPanel theyMessagePanel;
+    private JPanel myMessageArrowPanel;
+    private JPanel myMessageTopPanel;
+    private JPanel myMessageBottomPanel;
+    private JPanel myMessageTextPanel;
 
 
     public JPanel getRootPanel() {
@@ -65,17 +77,51 @@ public class ConversationsForm {
         accountIconPanel = new JPanelImage("res/your-face.png");
         accountSettingsIconPanel = new JPanelImage("res/icon-settings.png");
         accountNameLabel = new JLabel();
-        accountNameLabel.setFont(regularFont.deriveFont(17f));
+        accountNameLabel.setFont(regularFont.deriveFont(14f));
         partnerIconPanel = new JPanelImage("res/current-user.png");
         editPartnerPanel = new JPanelImage("res/icon-edit.png");
 
         user1AvatarPanel = new JPanelImage("res/user-avatar.png");
 
-        Border conversationsBottomBorder = BorderFactory.createMatteBorder(0,0,1,0,Color.BLACK);
+        Border conversationsRightBorder = BorderFactory.createMatteBorder(0,0,0,1,new Color(219,219,219));
+        conversationsListPanel = new JPanel();
+        conversationsListPanel.setBorder(conversationsRightBorder);
+
+        Border searchPanelRightBottomBorder = BorderFactory.createMatteBorder(0,0,1,1,new Color(219,219,219));
+        searchPanel = new JPanel();
+        searchPanel.setBorder(searchPanelRightBottomBorder);
+
+        searchLabel = new JLabel();
+        searchLabel.setFont(regularFont.deriveFont(16.0f));
+
+        Border chatPartnerBottomBorder = BorderFactory.createMatteBorder(0,0,1,0,new Color(237,237,227));
+        chatPartnerPanel = new JPanel();
+        chatPartnerPanel.setBorder(chatPartnerBottomBorder);
+
+
+        Border conversationsBottomBorder = BorderFactory.createMatteBorder(0,0,1,0,new Color(219,219,219));
         user1 = new JPanel();
         user1.setBorder(conversationsBottomBorder);
         user2 = new JPanel();
         user2.setBorder(conversationsBottomBorder);
+
+        user1Name = new JLabel();
+        user1Name.setFont(boldFont.deriveFont(16.0f));
+
+        user1LastMessage = new JLabel();
+        user1LastMessage.setFont(regularFont.deriveFont(12.0f));
+        user1when = new JLabel();
+        user1when.setFont(regularFont.deriveFont(10.0f));
+
+        partnerLabel = new JLabel();
+        partnerLabel.setFont(regularFont.deriveFont(14.0f));
+
+        myMessageTopPanel = new JPanelImage("res/message-out-top.png");
+        myMessageBottomPanel = new JPanelImage("res/message-out-bottom.png");
+        myMessageArrowPanel = new JPanelImage("res/message-out-right.png");
+
+        messageButtonPanel = new JPanelImage("res/button-send.png");
+
 
     }
 }
