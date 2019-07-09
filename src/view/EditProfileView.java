0b3c14407c4fbe85a45c720ce0c73c98bc9c6302
Paul <tsapree@gmail.com>
2019-07-09 20:03:13 +0500

    added some elements in edit profile view

--- src/view/EditProfileView.java
+++ src/view/EditProfileView.java
@@ -1,4 +1,16 @@
 package view;
 
+import javax.swing.*;
+
 public class EditProfileView {
+    private JPanel nameState;
+    private JLabel profileLabel;
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JPanel saveButtonBackground;
+    private JButton saveButton;
+    private JLabel profileProcessLabel;
+    private JButton backButton;
+    private JLabel exitButtonLabel;
+    private JLabel phoneLabel;
 }
